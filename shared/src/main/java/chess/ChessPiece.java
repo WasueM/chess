11338cfc0c3f3f50316124931ac5f6efc4654ef8
Wesley Mangum <wesley@mangumfamily.org>
2025-01-14 18:13:50 -0700

    A new function which detects if the possible move will be on or off the board

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -44,6 +44,21 @@ public class ChessPiece {
         return this.pieceType;
     }
 
+    // a function which takes in a chess position and a modification to it, and returns the new place on the board or null if its off the board
+    public ChessPosition makeNewPositionIfPossible(ChessPosition position, int upDown, int leftRight) {
+        int currentPositionRow = position.getRow();
+        int currentPositionColumn = position.getColumn();
+
+        int newPositionRow = currentPositionRow + upDown;
+        int newPositionColumn = currentPositionColumn + leftRight;
+
+        if (newPositionRow < 1 || newPositionRow > 8 || newPositionColumn < 1 || newPositionColumn > 8) {
+            return null;
+        } else {
+            return new ChessPosition(newPositionRow, newPositionColumn);
+        }
+    }
+
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -52,6 +67,54 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        // we will store possible moves here
+        ChessPosition[] possibleMoves = new ChessPosition[32]; // not sure if 32 is the right size but it'll be big enough for sure
+
+        // so I guess we'll do a switch statement for each possible piece that this could be to organize it?
+        switch (this.pieceType) {
+            case KING:
+                // can go any direction straight and diagonal on space, add these
+                // calculate and add each to that array
+                ChessPosition straightUp = makeNewPositionIfPossible(myPosition, 1, 0);
+                possibleMoves[0] = straightUp;
+                ChessPosition straightDown = makeNewPositionIfPossible(myPosition, -1, 0);
+                possibleMoves[1] = straightDown;
+                ChessPosition straightLeft = makeNewPositionIfPossible(myPosition, 0, -1);
+                possibleMoves[2] = straightLeft;
+                ChessPosition straightRight = makeNewPositionIfPossible(myPosition, 0, 1);
+                possibleMoves[3] = straightRight;
+                ChessPosition diagonalUpLeft = makeNewPositionIfPossible(myPosition, 1, -1);
+                possibleMoves[4] = diagonalUpLeft;
+                ChessPosition diagonalUpRight = makeNewPositionIfPossible(myPosition, 1, 1);
+                possibleMoves[5] = diagonalUpRight;
+                ChessPosition diagonalDownLeft = makeNewPositionIfPossible(myPosition, -1, -1);
+                possibleMoves[6] = diagonalDownLeft;
+                ChessPosition diagonalDownRight = makeNewPositionIfPossible(myPosition, -1, 1);
+                possibleMoves[7] = diagonalDownRight;
+                break;
+            case QUEEN:
+
+                break;
+            case BISHOP:
+
+                break;
+            case KNIGHT:
+
+                break;
+            case ROOK:
+
+                break;
+            case PAWN:
+
+                break;
+        }
+
+        // loop through them and remove them if they are null or occupied by a piece of the same color
+        for (ChessPosition possibleMove : possibleMoves) {
+            ChessPiece possibleMovePiece = board.getPiece(possibleMove);
+            if (possibleMovePiece.getTeamColor() != this.pieceColor) {
+                possibleMove = null; // set to null so we know we can't move there
+            }
+        }
     }
 }
