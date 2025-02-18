c3bd187d2497fb0aab48e1df389061ade64e11e8
Wesley Mangum <wesley@mangumfamily.org>
2025-02-18 14:56:59 -0700

    Getting set up with the server (mostly working)

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -9,10 +9,13 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
+        //Spark.staticFiles.externalLocation("src/main/resources/web");
+
         // Register your endpoints and handle exceptions here.
+        createEndpoints();
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        //Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -22,4 +25,44 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    private void createEndpoints() {
+        Spark.delete("/db", (request, response) -> {
+            return "HOWDY DAWGS";
+        });
+        Spark.post("/user", (request, response) -> {
+            return "HOWDY DAWGS";
+        });
+        Spark.post("/session", (request, response) -> {
+            return "HOWDY DAWGS";
+        });
+        Spark.delete("/session", (request, response) -> {
+            return "HOWDY DAWGS";
+        });
+        Spark.get("/game", (request, response) -> {
+            return "HOWDY DAWGS";
+        });
+        Spark.post("/game", (request, response) -> {
+            return "HOWDY DAWGS";
+        });
+        Spark.put("/game", (request, response) -> {
+            return "HOWDY DAWGS";
+        });
+    }
 }
+
+//public class MyServer {
+//    public static void main(String[] args) {
+//        try {
+//            createRoutes();
+//        } catch(ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+//            System.err.println("Specify the port number as a command line paramter");
+//        }
+//    }
+//    private void createRoutes() {
+//        Spark.get("/hi", (request, response) -> {
+//            return "HOWDY DAWGS";
+//            // request.params(":name");
+//        });
+//    }
+//}
