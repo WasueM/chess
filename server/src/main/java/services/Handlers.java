7d25e679f4c6e2da259117ffce0dab7b35e7f41f
Wesley Mangum <wesley@mangumfamily.org>
2025-03-02 11:41:55 -0700

    Continued refactor

--- server/src/main/java/services/Handlers.java
+++ server/src/main/java/services/Handlers.java
@@ -5,15 +5,24 @@ import com.google.gson.Gson;
 //record ServerResponse(int statusCode, String message) {}
 
 public class Handlers {
-    private static final Gson gson = new Gson();
-    private static final UserService userService = new UserService();
 
-    public static String handleRegister(String request, String response) {
+    private final UserService userService;
+    private final GameService gameService;
+    private final AuthService authService;
+
+    private static final Gson gson = new Gson();
+
+    public Handlers(UserService userService, GameService gameService, AuthService authService) {
+        this.userService = userService;
+        this.gameService = gameService;
+        this.authService = authService;
+    }
+
+    public String handleRegister(String request, String response) {
         RegisterRequest registerRequest = gson.fromJson(request, RegisterRequest.class);
         try {
             RegisterResult result = userService.register(registerRequest);
             if (result == null) {
-//                ServerResponse serverResponse = new ServerResponse(450, "Username already in use");
                 return gson.toJson("Username already in use");
             }
             return gson.toJson(result);
