cb325d03b0afeb7d582d3f3f28732c1ba6332315
Wesley Mangum <wesley@mangumfamily.org>
2025-03-26 14:51:38 -0600

    More and prettier info for printing the game

--- client/src/main/java/client/GameController.java
+++ client/src/main/java/client/GameController.java
@@ -17,6 +17,36 @@ public class GameController {
     }
 
     public void show() {
+        // "clear" the viewing space a bit
+        this.clear();
+
+        // print the usernames at the top
+        this.printGameInfo();
+
+        // drawing the board
+        this.drawBoard();
+    }
+
+    public void clear() {
+        System.out.print("\n\n\n\n\n\n\n\n\n\n"); // 10 new lines to push things out of view from earlier
+    }
+
+    public void printGameInfo() {
+        System.out.println("---------------");
+        this.printGameName();
+        this.printUsernames();
+        System.out.println("---------------");
+    }
+
+    public void printGameName() {
+        System.out.print("Game: " + this.game.gameName());
+    }
+
+    public void printUsernames() {
+        System.out.print("White: " + this.game.whiteUsername() + " | Black: " + this.game.blackUsername());
+    }
+
+    public void drawBoard() {
         if (playerColor == ChessGame.TeamColor.WHITE) {
             // draw it with white at the bottom
             System.out.print(game.game().chessBoard);
@@ -24,5 +54,9 @@ public class GameController {
             // draw it with black at the bottom
             System.out.print(game.game().chessBoard.toStringInverted());
         }
+    }
+
+    public void highlightLegalMoves() {
+
     }
 }
